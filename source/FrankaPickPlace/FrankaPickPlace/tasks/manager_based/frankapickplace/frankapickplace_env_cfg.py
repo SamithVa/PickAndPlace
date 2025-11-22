1dc8a6aa1b8f56f22e2b471ade7273544f697708
SamithVA <vasamith@163.com>
2025-11-22 13:40:00 +0800

    add place reward function

--- source/FrankaPickPlace/FrankaPickPlace/tasks/manager_based/frankapickplace/frankapickplace_env_cfg.py
+++ source/FrankaPickPlace/FrankaPickPlace/tasks/manager_based/frankapickplace/frankapickplace_env_cfg.py
@@ -17,17 +17,16 @@ from isaaclab.managers import SceneEntityCfg
 from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.utils import configclass
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 
 from isaaclab_assets import FRANKA_PANDA_CFG  # isort: skip
 from isaaclab.markers.config import FRAME_MARKER_CFG  # isort: skip
 from isaaclab.assets import RigidObjectCfg
 from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
-from isaaclab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
 from isaaclab.sensors.frame_transformer.frame_transformer_cfg import FrameTransformerCfg
 from isaaclab.sensors.frame_transformer.frame_transformer_cfg import OffsetCfg
-
 from . import mdp
+
+
 ##
 # Scene definition
 ##
@@ -48,36 +47,42 @@ class FrankaSceneCfg(InteractiveSceneCfg):
 
     # objects
     object = RigidObjectCfg(
-            prim_path="{ENV_REGEX_NS}/Cube",
-            init_state=RigidObjectCfg.InitialStateCfg(pos=[0.5, 0, 0.055], rot=[1, 0, 0, 0]),
-            spawn=sim_utils.CuboidCfg(
-                size=[0.05, 0.05, 0.05],
-                semantic_tags=[('color', 'red')],
-                rigid_props=RigidBodyPropertiesCfg(
-                    solver_position_iteration_count=16,
-                    solver_velocity_iteration_count=1,
-                    max_angular_velocity=1000.0,
-                    max_linear_velocity=1000.0,
-                    max_depenetration_velocity=5.0,
-                    disable_gravity=False, 
-                ),
-                visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0), metallic=0.2),
-                collision_props=sim_utils.CollisionPropertiesCfg(),
-                physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=1.0)
+        prim_path="{ENV_REGEX_NS}/Cube",
+        init_state=RigidObjectCfg.InitialStateCfg(pos=[0.2, 0, 0.05], rot=[1, 0, 0, 0]),
+        spawn=sim_utils.CuboidCfg(
+            size=[0.05, 0.05, 0.05],
+            semantic_tags=[("color", "red")],
+            rigid_props=RigidBodyPropertiesCfg(
+                solver_position_iteration_count=16,
+                solver_velocity_iteration_count=1,
+                max_angular_velocity=1000.0,
+                max_linear_velocity=1000.0,
+                max_depenetration_velocity=5.0,
+                disable_gravity=False,
             ),
-        )
+            visual_material=sim_utils.PreviewSurfaceCfg(
+                diffuse_color=(0.0, 1.0, 0.0), metallic=0.2
+            ),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=1.0),
+        ),
+    )
 
     # Listens to the required transforms, adding visualization markers to end-effector frame
     ee_frame = FrameTransformerCfg(
-        prim_path="{ENV_REGEX_NS}/Robot/panda_link0", # root of robot (base_link)
+        prim_path="{ENV_REGEX_NS}/Robot/panda_link0",  # root of robot (base_link)
         debug_vis=False,
         visualizer_cfg=FRAME_MARKER_CFG.copy().replace(
-            markers={"frame": FRAME_MARKER_CFG.markers["frame"].replace(scale=(0.1, 0.1, 0.1))},
-            prim_path="/Visuals/FrameTransformer"
+            markers={
+                "frame": FRAME_MARKER_CFG.markers["frame"].replace(
+                    scale=(0.1, 0.1, 0.1)
+                )
+            },
+            prim_path="/Visuals/FrameTransformer",
         ),
         target_frames=[
             FrameTransformerCfg.FrameCfg(
-                prim_path="{ENV_REGEX_NS}/Robot/panda_hand", # end-effector link
+                prim_path="{ENV_REGEX_NS}/Robot/panda_hand",  # end-effector link
                 name="end_effector",
                 offset=OffsetCfg(
                     pos=[0.0, 0.0, 0.1034],
@@ -85,7 +90,6 @@ class FrankaSceneCfg(InteractiveSceneCfg):
             ),
         ],
     )
-
 
     # lights
     light = AssetBaseCfg(
@@ -102,20 +106,20 @@ class FrankaSceneCfg(InteractiveSceneCfg):
 @configclass
 class CommandsCfg:
     """Command terms for the MDP."""
-    
-    ### Added for pick and place task ###
+
+    # Place location (where to drop the cube)
     drop_pose = mdp.UniformPoseCommandCfg(
         asset_name="robot",
-        body_name="panda_hand",  # will be set by agent env cfg
-        resampling_time_range=(5.0, 5.0),  # Match episode_length_s
+        body_name="panda_hand",
+        resampling_time_range=(8.0, 8.0),
         debug_vis=True,
         ranges=mdp.UniformPoseCommandCfg.Ranges(
-            pos_x=(0.3, 0.6),      
-            pos_y=(-0.4, 0.4),
-            pos_z=(0.2, 0.2),  
-            roll=(0.0, 0.0), 
-            pitch=(0.0, 0.0), 
-            yaw=(0.0, 0.0)
+            pos_x=(0.4, 0.6),  # Where to place
+            pos_y=(-0.25, 0.25),
+            pos_z=(0.25, 0.5),  # Placement height
+            roll=(0.0, 0.0),
+            pitch=(0.0, 0.0),
+            yaw=(0.0, 0.0),
         ),
     )
 
@@ -125,13 +129,16 @@ class ActionsCfg:
     """Action specifications for the MDP."""
 
     arm_action: ActionTerm = mdp.JointPositionActionCfg(
-            asset_name="robot", joint_names=["panda_joint.*"], scale=0.5, use_default_offset=True
+        asset_name="robot",
+        joint_names=["panda_joint.*"],
+        scale=0.5,
+        use_default_offset=True,
     )
     gripper_action: ActionTerm = mdp.BinaryJointPositionActionCfg(
-            asset_name="robot",
-            joint_names=["panda_finger.*"],
-            open_command_expr={"panda_finger_.*": 0.04},
-            close_command_expr={"panda_finger_.*": 0.0},
+        asset_name="robot",
+        joint_names=["panda_finger.*"],
+        open_command_expr={"panda_finger_.*": 0.04},
+        close_command_expr={"panda_finger_.*": 0.0},
     )
 
 
@@ -143,12 +150,14 @@ class ObservationsCfg:
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
 
-        joint_pos = ObsTerm(func=mdp.joint_pos_rel) # 9 
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel) # 9
-        object_position = ObsTerm(func=mdp.object_position_in_robot_root_frame) # 3 + 4 = 7 (xyz, quat)
-        ### Added for pick and place task
-        target_drop_position = ObsTerm(func=mdp.generated_commands, params={"command_name": "drop_pose"}) # 3 
-        actions = ObsTerm(func=mdp.last_action) # 9 + 2 (end-effector) = 8
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+        object_position = ObsTerm(func=mdp.object_position_in_robot_root_frame)
+
+        place_target = ObsTerm(
+            func=mdp.generated_commands, params={"command_name": "drop_pose"}
+        )
+        actions = ObsTerm(func=mdp.last_action)
 
         def __post_init__(self):
             self.enable_corruption = True
@@ -168,7 +177,7 @@ class EventCfg:
         func=mdp.reset_root_state_uniform,
         mode="reset",
         params={
-            "pose_range": {"x": (0.1, 0.25), "y": (-0.1, 0.1), "z": (0.0, 0.0)},
+            "pose_range": {"x": (-0.1, 0.1), "y": (-0.25, 0.25), "z": (0.0, 0.0)},
             "velocity_range": {},
             "asset_cfg": SceneEntityCfg("object", body_names="Cube"),
         },
@@ -179,49 +188,86 @@ class EventCfg:
 class RewardsCfg:
     """Reward terms for the MDP."""
 
-    # PHASE 1: PICKING
-    reaching_object = RewTerm(func=mdp.object_ee_distance, params={"std": 0.1}, weight=1.0)
-    lifting_object = RewTerm(func=mdp.object_is_lifted, params={"minimal_height": 0.04}, weight=15.0)
-
-    # PHASE 2: MOVING TO TARGET
-    object_goal_tracking = RewTerm(
-        func=mdp.object_goal_distance,
-        params={"std": 0.3, "minimal_height": 0.2, "command_name": "drop_pose"},
-        weight=16.0,
+    # Stage 1: Reaching
+    reaching_object = RewTerm(
+        func=mdp.object_ee_distance, params={"std": 0.1}, weight=1.0
     )
 
-    # When close to the goal, provide a finer-grained reward to encourage precise placement
-    object_goal_tracking_fine_grained = RewTerm(
-        func=mdp.object_goal_distance,
-        params={"std": 0.05, "minimal_height": 0.2, "command_name": "drop_pose"},
-        weight=5.0,
+    # Stage 2: Grasping
+    grasping_object = RewTerm(
+        func=mdp.grasp_reward, 
+        params={"distance_threshold": 0.04},
+        weight=5.0
     )
-    
-    # PHASE 3: PLACING
-    # gentle_placing = RewTerm(
-    #     func=mdp.placing_object_gentle,
-    #     params={"command_name": "drop_pose", "target_height": 0.05},
-    #     weight=30.0,
+
+    # Stage 3: Lifting
+    lifting_object = RewTerm(
+        func=mdp.object_is_lifted, params={"minimal_height": 0.04}, weight=10.0
+    )
+
+    # Stage 4: Transport (multi-scale for better gradients)
+    object_goal_tracking_coarse = RewTerm(
+        func=mdp.object_goal_distance,
+        params={"std": 0.5, "minimal_height": 0.04, "command_name": "drop_pose"},
+        weight=8.0,
+    )
+
+    object_goal_tracking_fine = RewTerm(
+        func=mdp.object_goal_distance,
+        params={"std": 0.1, "minimal_height": 0.04, "command_name": "drop_pose"},
+        weight=8.0,
+    )
+
+    # Stage 5: Placement (height-aware)
+    placement_reward = RewTerm(
+        func=mdp.placement_height_reward,
+        params={"xy_threshold": 0.08, "target_height_offset": 0.05, "command_name": "drop_pose"},
+        weight=15.0,
+    )
+
+    # Stage 6: Release
+    release_reward = RewTerm(
+        func=mdp.release_reward,
+        params={"xy_threshold": 0.05, "height_threshold": 0.08, "command_name": "drop_pose"},
+        weight=20.0,
+    )
+
+    # reward_reach = RewTerm(
+    #     mdp.reward_stage_reach,
+    #     weight=1.0,
     # )
 
-    # COMBINED PICK AND PLACE REWARD
-    # picking_and_placing = RewTerm(
-    #     func=mdp.staged_pick_and_place_reward,
-    #     params={
-    #         "std": 0.25,
-    #         "command_name": "drop_pose"
-    #     },
-    #     weight=1e-1,
+    # rew_lift = RewTerm(
+    #     mdp.reward_stage_lift,
+    #     params={"min_height": 0.04},
+    #     weight=0.0,
+    # )
+
+    # rew_transport = RewTerm(
+    #     mdp.reward_stage_transport,
+    #     params={"std": 0.2,"min_height": 0.04, "command_name": "drop_pose"},
+    #     weight=0.0,
+    # )
+
+    # rew_transport_fine_grained = RewTerm(
+    #     mdp.reward_stage_transport,
+    #     params={"std": 0.05,"min_height": 0.04, "command_name": "drop_pose"},
+    #     weight=0.0,
+    # )
+
+    # rew_place = RewTerm(
+    #     mdp.reward_stage_place,
+    #     params={"distance_threshold": 0.03, "command_name": "drop_pose"},
+    #     weight=0.0,
     # )
 
     # --- PENALTIES --- #
-    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-5e-5)
+    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-1e-4)
     joint_vel = RewTerm(
         func=mdp.joint_vel_l2,
-        weight=-5e-5,
+        weight=-1e-4,
         params={"asset_cfg": SceneEntityCfg("robot")},
     )
-
 
 
 @configclass
@@ -230,19 +276,35 @@ class TerminationsCfg:
 
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
 
-    object_reaching_goal = DoneTerm(func=mdp.object_reached_goal, params={"command_name": "drop_pose"})
+    # object_reaching_goal = DoneTerm(
+    #     func=mdp.object_reached_goal, params={"command_name": "drop_pose"}
+    # )
 
 
 @configclass
 class CurriculumCfg:
     """Curriculum terms for the MDP."""
 
+    # Gradually increase placement and release rewards as training progresses
+    placement_weight = CurrTerm(
+        func=mdp.modify_reward_weight,
+        params={"term_name": "placement_reward", "weight": 30.0, "num_steps": 15000},
+    )
+
+    release_weight = CurrTerm(
+        func=mdp.modify_reward_weight,
+        params={"term_name": "release_reward", "weight": 40.0, "num_steps": 25000},
+    )
+
+    # Gradually increase action penalties to encourage smooth motion
     action_rate = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -1e-3, "num_steps": 100000}
+        func=mdp.modify_reward_weight,
+        params={"term_name": "action_rate", "weight": -1e-2, "num_steps": 20000},
     )
 
     joint_vel = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -1e-3, "num_steps": 100000}
+        func=mdp.modify_reward_weight,
+        params={"term_name": "joint_vel", "weight": -1e-2, "num_steps": 20000},
     )
 
 
@@ -270,15 +332,17 @@ class FrankPickPlaceEnvCfg(ManagerBasedRLEnvCfg):
         # general settings
         self.decimation = 2
         self.sim.render_interval = self.decimation
-        self.episode_length_s = 5.0  # Increased from 5.0 to allow time for pick and place
+        self.episode_length_s = (
+            5.0  # Increased from 5.0 to allow time for pick and place
+        )
         # simulation settings
         self.sim.dt = 0.01
-        self.sim.physx.bounce_threshold_velocity = 0.2
+        self.sim.physx.bounce_threshold_velocity = 0.01
         self.sim.physx.gpu_found_lost_aggregate_pairs_capacity = 1024 * 1024 * 4
         self.sim.physx.gpu_total_aggregate_pairs_capacity = 16 * 1024
         self.sim.physx.friction_correlation_distance = 0.00625
 
-        
+
 @configclass
 class FrankPickPlaceCfgEnvCfg_PLAY(FrankPickPlaceEnvCfg):
     def __post_init__(self):
